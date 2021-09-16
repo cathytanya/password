8b416697ec3d696dd36c567b1f57e1fea88203a9
cathytanya <cathycolette.tanya@gmail.com>
2021-09-16 14:48:08 -0400

    putting asome comments giving an outline of how the code should layout

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 // Assignment Code
 let generateBtn = document.querySelector("#generate");
 // setting the characters needed for the generator to variables
-let letters = "abcdefghijklmnopqrstuvwxyz"
+let lettersLower = "abcdefghijklmnopqrstuvwxyz"
+let lettersUpper = "ABCDEFGHIJKLMNOPQRSTUV"
 let numbers = "0123456789"
 let char = "!@#$%^&*()_=+[]\{}|<>?/;':-"
+let password=document.getElementById("password")
 
 
-//
+// prompt where people will enter the the length of their password
+// also the password can only have a length between 8-128
+
 function promptCriteria() { 
   alert("Which criteria does your passwork need. Select the following options:") 
 }
