735845b4678f4b0dab88cd9fb042a5f5ce716884
cathytanya <cathycolette.tanya@gmail.com>
2021-09-16 16:21:02 -0400

    formed the length code for the password generator

--- script.js
+++ script.js
@@ -1,19 +1,48 @@
 // Assignment Code
 let generateBtn = document.querySelector("#generate");
 // setting the characters needed for the generator to variables
-let lettersLower = "abcdefghijklmnopqrstuvwxyz"
-let lettersUpper = "ABCDEFGHIJKLMNOPQRSTUV"
-let numbers = "0123456789"
-let char = "!@#$%^&*()_=+[]\{}|<>?/;':-"
+let lettersLower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+let lettersUpper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+let numbers = ["0","1","2","3","4","5","6","7","8","9"]
+let char = ["!","@","#","$","%","^","&","*","(",")","_","=","+",",","<",">","?","-"]
 let password=document.getElementById("password")
 
+function promptCriteria() {
+  let length = parseInt (window.prompt("how long"));
+  
+  if (length < 8){
+    window.alert("NEEDS TO BE MORE THAN 8")
+  
+    return
+  }
+  if (length > 128){
+    window.alert("NEEDS TO BE LESS THAN 128")
+
+    return
+  }
+  // CHECK IF ITS A NUMBER
+  if (Number.isNaN(length)){
+    window.alert("PASSWORD LENGTH NEEDS TO BE A NUMBER")
+    
+    return
+  }
+  
+  var confirmlength = confirm("You want your password too be this "+ length + " many characters?")
+  
+  
+  var confirmLower = confirm("Are you sure that you want lower CASE")
+  var confirmUpper = confirm("Do you want Upper case ")
+  var confirmNumber = confirm("do you want numbers")
+  var confirmSpecial = confirm("do you want special characters")
+}
+
+function generatePassword(){
+  let choice = promptCriteria()
+}
 
 // prompt where people will enter the the length of their password
 // also the password can only have a length between 8-128
 
-function promptCriteria() { 
-  alert("Which criteria does your passwork need. Select the following options:") 
-}
 
 // Write password to the #password input
 function writePassword() {
