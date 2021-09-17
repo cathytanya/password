3cf3a9ccd82e63df3ae6a9b9b03c5951dda5598e
cathytanya <cathycolette.tanya@gmail.com>
2021-09-17 14:44:29 -0400

    removed console log and made the password referrenced on line 92 of javascript into an ID

--- script.js
+++ script.js
@@ -88,7 +88,8 @@ function generatePassword(){
 // Write password to the #password input
 function writePassword() {
   let password = generatePassword();
-  let passwordText = document.querySelector("password");
+  
+  let passwordText = document.querySelector("#password");
   passwordText.value = password;
   return
 }
